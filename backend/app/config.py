117888bd205e91b83f40e49858e92d8ec1cc0a50
Minh Quang <quangm279@gmail.com>
2026-06-30 22:30:25 +0700

    test update backend version

--- backend/app/config.py
+++ backend/app/config.py
@@ -5,7 +5,7 @@ from dataclasses import dataclass
 @dataclass(frozen=True)
 class Settings:
     port: int = int(os.getenv("PORT", "8080"))
-    app_version: str = os.getenv("APP_VERSION", "v4.0.0")
+    app_version: str = os.getenv("APP_VERSION", "v5.0.0")
     service_name: str = os.getenv("SERVICE_NAME", "backend")
 
 
